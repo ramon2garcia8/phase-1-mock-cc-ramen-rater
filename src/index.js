4c22c6498f38458d4e418a5e8ca3aad2850f78ed
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-06-27 15:57:13 -0400

    mock cc ramen rater

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // write your code here
+const ramenDiv = document.getElementById("ramen-menu")
+const ramenDetail = document.getElementById("ramen-detail")
+
+function getRamenImgsFromServer() {
+    fetch("http://localhost:3000/ramens")
+    .then(res => res.json())
+    .then(ramenArray => {
+        ramenArray.forEach(ramenObj => {
+            const ramenImg = document.createElement("img")
+            ramenImg.addEventListener('click', () => {
+                const detailName = ramenDetail.querySelector(".name")
+                const detailRestaurant = ramenDetail.querySelector(".restaurant")
+                const detailImg = ramenDetail.querySelector(".detail-image")
+                detailName.textContent = ramenObj.name
+                detailRestaurant.textContent = ramenObj.restaurant 
+                detailImg.src = ramenObj.image
+            })
+            ramenImg.src = ramenObj.image
+            ramenDiv.append(ramenImg)
+        }) 
+    })
+}
+
+getRamenImgsFromServer();
