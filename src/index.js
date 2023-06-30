5ff1b7ace2e0fb28ddf16c4d938a80fb38709bc9
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-06-29 20:03:45 -0400

    mock cc ramen rater

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 // write your code here
 const ramenDiv = document.getElementById("ramen-menu")
 const ramenDetail = document.getElementById("ramen-detail")
+const ramenForm = document.getElementById("new-ramen")
+
+ramenForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newRamen = createRamenObject(e)
+    postRamen(newRamen)
+} )
+
+function createRamenObject(e) {
+    return {
+        name: e.target["name"].value,
+        restaurant: e.target["restaurant"].value,
+        image: e.target["image"].value,
+        rating: e.target["rating"].value,
+        comment: e.target["new-comment"].value
+    }
+}
+
+function postRamen(newRamen) {
+    console.log(JSON.stringify(newRamen))
+    const config = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newRamen)
+    }
+    fetch("http://localhost:3000/ramens", config)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
 
 function getRamenImgsFromServer() {
     fetch("http://localhost:3000/ramens")
